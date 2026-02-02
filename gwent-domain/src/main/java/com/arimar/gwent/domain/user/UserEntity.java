ce4204055727db64543f071f9b6fb0c62eca6255
Arielito <arimarquez2444@gmail.com>
2026-02-02 18:27:59 -0300

    Agregar entidades de Usuario y Jugador

---
+++ gwent-domain/src/main/java/com/arimar/gwent/domain/user/UserEntity.java
@@ -0,0 +1,40 @@
+package com.arimar.gwent.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "gw_user")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "game_id")
+    private String gameId;
+
+    @Column(name = "user_code")
+    private String userCode;
+
+}
